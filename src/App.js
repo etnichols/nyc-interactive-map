394b7f16a01b94edc9ff6360c170b50d0a3e41cb
Evan Nichols <evannichols@MacBook-Pro-28.local>
2017-11-19 20:45:16 -0500

    simple animations, stuff

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 import FiveBoroughs from './FiveBoroughs';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>NYC Interactive Map</h1>
+      <div className="container-fluid">
+        <h2>5 Boroughs + NYC Neighborhoods</h2>
+        <div className="row">
         <FiveBoroughs/>
+        </div>
       </div>
     );
   }
