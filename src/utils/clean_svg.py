925415eab22d00a56ead40960f5a23364d2c91fc
Evan Nichols <evantnichols@gmail.com>
2018-01-24 21:21:17 -0500

    tweaks

--- src/utils/clean_svg.py
+++ src/utils/clean_svg.py
@@ -5,16 +5,12 @@ import re
 def clean_opening_tag(line):
     line = re.sub(r'data-name=\".+?\"', "className=\"manhattan-svg-container\"", line)
     line = re.sub(r'xmlns=\".+?\"', "ref={node => this.node = node}", line)
-
-    # and add ref, claaName
-
-    # could update the id but eh
     return line
 
-# Make "SOMETHING_LIKE_THIS" to "Something Like This"
+# Format the ID to remove all caps and underscores.
+# "SOMETHING_LIKE_THIS" ==> "Something Like This"
 def clean_tag_id(str):
     str = str.replace("_"," ")
-    # use "title to get" "Title To Get"
     return str.title()
 
 # things that need to be cleaned, <polygon, <path, <rect, <g
@@ -24,12 +20,14 @@ def clean_tag(tag):
     # if it doesn't have an id, no updates necessary, return
     if "<g" not in tag:
         return tag
-    # ok so let's grab the matches
+
+    # Use regex to grab all quoted strings in the
     matches=re.findall(r'\"(.+?)\"',tag)
     tag_id = matches[0]
+
     class_name = "" if "class=" not in tag else matches[1]
 
-    # test injection
+    # Injection is used to attach event listeners and conditional styling to an element.
     injection = "\n onClick={this.onClickHandle} \n onMouseEnter={this.onClickHandle} \n className={\"" + class_name + "\" + (selection === \"" + tag_id + "\" ? \" selected\" : \"\")}"
 
     clean_line = re.sub(r'id=\"(.+?)\"', "id=\"" + tag_id + "\"" + injection, tag)
@@ -39,12 +37,12 @@ def clean_tag(tag):
     return clean_line
 
 def main():
-    # Init output files
+    # Init output files.
     output_file = open(sys.argv[2],"w+")
     css_output_file = open(sys.argv[3], "w+")
 
     with open(sys.argv[1],"r") as input:
-        #create the opening tag
+        # Create the opening tag
         opening_tag = input.readline()
         output_file.write(clean_opening_tag(opening_tag))
 
@@ -63,17 +61,16 @@ def main():
         # consume closing </defs> tag
         input.readline()
 
-        # save some time, wrap entire thing in g for d3 access.
+        # Wrap entire svg in g tag, for d3 access.
         output_file.write("<g id=\"the_map\">\n")
 
-        # and now process remaining svg body, letting the clean_tag function do the work.
+        # And now process remaining svg body, letting the clean_tag function do the work.
         for line in input.readlines():
             if "</svg>" not in line:
                 output_file.write(clean_tag(line))
             else:
                 output_file.write("</g>\n</svg>")
 
-        # close input, we done
         input.close()
 
 if __name__ == "__main__":
